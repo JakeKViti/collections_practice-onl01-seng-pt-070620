d258f2ca7e075d7b46c130422c0de1b731d90a2a
Jacob Viti <thevitij64@gmail.com>
2020-07-15 19:28:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort |a, b|
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    2 <=> 3
+  end
+end
+
+def reverse_array(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def kesha_maker(array)
+
+end
+
+def find_a(array)
+
+end
+
+def sum_array(array)
+
+end
+
+def add_s(array)
+
+end
